986efac3f1cb1be5213449d67a5816def828bbfd
MurshidMac <mh.murshidco.uk@outlook.com>
2017-05-03 19:09:58 +0530

    Global variables

--- Essential Notes/lesson One/global.js
+++ Essential Notes/lesson One/global.js
@@ -1,3 +1,6 @@
 console.log("Murshid Hassen");			// To run the file node global.js
-global.console.log("this is a global object");	//console object is available as a global object, this will work
+global.console.log("this is a global object");	//console object is available as a global object(Name space), this will work
 						// both are the same in sense
+var murshidvalue="Hi I am Murshid";		// this is global variable declarations
+console.log(global.murshidvalue);		// we will get something like undefined
+						// Be cause nodejs works something different to when it stores global variables
