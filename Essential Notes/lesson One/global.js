aff91990d16de12bd2b23d4a2f44108b93d22b80
MurshidMac <mh.murshidco.uk@outlook.com>
2017-05-03 19:04:09 +0530

    Added the Console Object

---
+++ Essential Notes/lesson One/global.js
@@ -0,0 +1,3 @@
+console.log("Murshid Hassen");			// To run the file node global.js
+global.console.log("this is a global object");	//console object is available as a global object, this will work
+						// both are the same in sense
