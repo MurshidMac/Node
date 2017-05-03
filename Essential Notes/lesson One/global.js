7d8242c75a94d58e5108f40e2dfdf93d9a136559
MurshidMac <mh.murshidco.uk@outlook.com>
2017-05-03 19:12:42 +0530

    added the global variable

--- Essential Notes/lesson One/global.js
+++ Essential Notes/lesson One/global.js
@@ -3,4 +3,6 @@ global.console.log("this is a global object");	//console object is available as 
 						// both are the same in sense
 var murshidvalue="Hi I am Murshid";		// this is global variable declarations
 console.log(global.murshidvalue);		// we will get something like undefined
-						// Be cause nodejs works something different to when it stores global variables
+						// Be cause nodejs works something different to when it stores global variables 
+console.log(murshidvalue);			// this is not in the global scope
+						// Now we can see that it prints the string in the value
