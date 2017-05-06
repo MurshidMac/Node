593001aa58c2d5b39db33c991dabd66533dd77e4
MurshidMac <mh.murshidco.uk@outlook.com>
2017-05-06 23:38:24 +0530

    here we come to the crazy functional part of returning the next element

--- Essential Notes/lesson One/app.js
+++ Essential Notes/lesson One/app.js
@@ -3,3 +3,9 @@ console.log(process.argv);          // Process Variable is store for any app wit
                                     // --user "murshid" --greeting "Murshid Say Hi"
                                     // This can be anything This is automatically added to process.argv variable during runtime
                                     // Ex node app.js --name MurshidHassen --age 18
+
+// Here comes the magic of functional programming
+function grab(flag){                                            // this will take a greeting as a flag
+    var index= process.argv.indexOf(flag);                      // here we are getting the index of the flag 
+    return (index === -1) ? null : process.argv[index+1];       // we are checking weather the index equalls to negative else we are returning the     
+}
