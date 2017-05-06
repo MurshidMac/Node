0d2736db3347a4a71a69b4f93cd49bd0ee0e46e0
MurshidMac <mh.murshidco.uk@outlook.com>
2017-05-06 23:30:18 +0530

    added the process.argv manually values during the runtime for the array

--- Essential Notes/lesson One/app.js
+++ Essential Notes/lesson One/app.js
@@ -1,1 +1,5 @@
 console.log(process.argv);          // Process Variable is store for any app with ARGV we can access the current process
+                                    // Similary we can add values to this object by adding something like 
+                                    // --user "murshid" --greeting "Murshid Say Hi"
+                                    // This can be anything This is automatically added to process.argv variable during runtime
+                                    // Ex node app.js --name MurshidHassen --age 18
