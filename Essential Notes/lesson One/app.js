bdd39ae1d6affba7b5f2013a752f2327b8a585d8
MurshidMac <mh.murshidco.uk@outlook.com>
2017-05-06 23:41:04 +0530

    added the call function

--- Essential Notes/lesson One/app.js
+++ Essential Notes/lesson One/app.js
@@ -9,3 +9,5 @@ function grab(flag){                                            // this will tak
     var index= process.argv.indexOf(flag);                      // here we are getting the index of the flag 
     return (index === -1) ? null : process.argv[index+1];       // we are checking weather the index equalls to negative else we are returning the     
 }
+var variable =grab("Murshid Hassen");                           // here I am passing the value
+console.log(variable);                                          // this should return us the index of the process function
