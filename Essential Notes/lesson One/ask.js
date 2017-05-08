4f460e608e6ec4fb1cbbf061744dee4cbd771b84
MurshidMac <mh.murshidco.uk@outlook.com>
2017-05-08 22:01:14 +0530

    added the stdout object and the special charactors

--- Essential Notes/lesson One/ask.js
+++ Essential Notes/lesson One/ask.js
@@ -1,1 +1,6 @@
 // standerd Inputs for JS file
+// Using the process fucntion
+
+//console.log("") // this is the standard output object of the JS
+process.stdout.write("My Name is Murshid \n");  // this is the standard output Object of the Terminal
+process.stdout.write("Murshid \n \nlest ");    // this won't give us a new line charactor   
