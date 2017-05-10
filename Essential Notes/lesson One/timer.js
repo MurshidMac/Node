e45df3835cd4ee612c1cfd4f1efae0e7a1c2958f
MurshidMac <mh.murshidco.uk@outlook.com>
2017-05-10 11:41:04 +0530

    added the percentage calcualation

--- Essential Notes/lesson One/timer.js
+++ Essential Notes/lesson One/timer.js
@@ -5,16 +5,24 @@
 var waitTime= 3000;     // 3 seconds long
 var currentTime=0;
 var waitInterval=500;
-
+var percentWait=0;
 
 
 console.log("wait for some time: ")
 
+// we will display the function in percentage
+function writeWaitPercentage(percent){
+    process.stdout.clearLine();     // this will clear the last Line in the terminal
+    process.stdout.cursorTo(0);     // this move the object to beginning of the Line
+    process.stdout.write(`Percentage ${percent}`);
+}
 
 
 // this is going to be our delay or wait time function
 setTimeout(function (){
     clearInterval(timeinterval);
+    writeWaitPercentage(100);                                       // here is the magic of mathematics begins
+    console.log("\n");              // added a new line charactor
     console.log("Done waiting")     // Invoke after Event Time
     clearInterval(timeinterval);    // this stop the interval
 }, waitTime);
@@ -24,9 +32,13 @@ setTimeout(function (){
 // what i am going to follow is to implement a variable which can stop during 3 seconds
 var timeinterval = setInterval(function (){
     currentTime+= waitInterval;
+    percentWait=Math.floor((currentTime/waitTime)*100);             // this is calculating the time  
     console.log(`Our Current Time is ${currentTime}`);              // this is not in seconds
     console.log(`Our Current Time in seconds  ${currentTime/1000}`);     // this is our current Time
+    writeWaitPercentage(percentWait);                                    // this will do the job again
+    console.log("% \n");                                                  // this is a newline charactor Peep
 }, waitInterval);                                                   // be careful this should be the wait interval ! waitime
 
-
+process.stdout.write("\n\n\n");         // this uses the new line functions
+writeWaitPercentage(percentWait)        // this converts to percentage
 
