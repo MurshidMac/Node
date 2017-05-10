fcaaa6d48048784695981ebec6124d815c4ca2ac
MurshidMac <mh.murshidco.uk@outlook.com>
2017-05-10 10:32:51 +0530

    added the asynchronous time function

---
+++ Essential Notes/lesson One/timer.js
@@ -0,0 +1,16 @@
+// we will be working with the timer functions to know more on JS
+// Asynchronous functionalities
+// https://nodejs.org/api/globals.html
+
+var waitTime= 3000;     // 3 seconds long
+
+console.log("wait for some time: ")
+
+// this is going to be our delay or wait time function
+setTimeout(function (){
+    console.log("Done waiting")     // Invoke after Event Time
+}, waitTime);
+
+
+
+
