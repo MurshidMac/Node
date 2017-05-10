eb505a617ecf980fa2953a2f8f8a500aec6bb45a
MurshidMac <mh.murshidco.uk@outlook.com>
2017-05-10 11:18:05 +0530

    added the clearInterval()

--- Essential Notes/lesson One/timer.js
+++ Essential Notes/lesson One/timer.js
@@ -10,16 +10,23 @@ var waitInterval=500;
 
 console.log("wait for some time: ")
 
+
+
 // this is going to be our delay or wait time function
 setTimeout(function (){
+    clearInterval(timeinterval);
     console.log("Done waiting")     // Invoke after Event Time
+    clearInterval(timeinterval);    // this stop the interval
 }, waitTime);
 
 // Setting an interval between executions
-setInterval(function (){
-    currentTime= currentTime+waitInterval;
+// notice here this code will run forever because it has no stop timings
+// what i am going to follow is to implement a variable which can stop during 3 seconds
+var timeinterval = setInterval(function (){
+    currentTime+= waitInterval;
     console.log(`Our Current Time is ${currentTime}`);              // this is not in seconds
     console.log(`Our Current Time in seconds  ${currentTime/1000}`);     // this is our current Time
-}, waitTime);
+}, waitInterval);                                                   // be careful this should be the wait interval ! waitime
+
 
 
