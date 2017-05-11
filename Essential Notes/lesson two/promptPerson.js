43dd7cc2c514dfbd41a10df697b47f90016a8be3
MurshidMac <mh.murshidco.uk@outlook.com>
2017-05-11 11:05:50 +0530

    added the readline to maintain the listners

---
+++ Essential Notes/lesson two/promptPerson.js
@@ -0,0 +1,17 @@
+// we will be considering some wrapper like readline()
+// its best to see the doc https://nodejs.org/api/readline.html
+var read= require('readline');              // this is to require the readline function in NodeJS
+var rl= read.createInterface(process.stdout, process.stdin);    // this is to maintain the listner Object with readline
+
+var JSObjectPerson={
+    name: '',
+    charactors:[]
+};                              // JS object to Store values    
+
+rl.question("What is your name? ",function (anwser){
+    JSObjectPerson.name=anwser;                                             // I am setting the name of the function
+    rl.setPrompt(`What would be ${JSObjectPerson.name} say`);               // template String    
+    console.log(anwser);
+});                             // a question to be asked
+//console.log(read);            // this is just to check the functions related to the readline Module
+
