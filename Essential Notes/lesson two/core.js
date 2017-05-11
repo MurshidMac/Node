73ed90eee9e7fc689a55d8b3781811ab101ab74d
MurshidMac <mh.murshidco.uk@outlook.com>
2017-05-11 10:26:36 +0530

    added the path basename function

--- Essential Notes/lesson two/core.js
+++ Essential Notes/lesson two/core.js
@@ -1,3 +1,4 @@
 // To Load Core or npm Modules(Custom JS files) to Our Program we Use the Require() function
 var path = require('path');         // getting the path module
 
+console.log(path.basename(__filename));     // this will return the base module name
