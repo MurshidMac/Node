569065eea943f3475d69e6f3e834b68c2d970142
MurshidMac <mh.murshidco.uk@outlook.com>
2017-05-11 10:43:49 +0530

    Added the coolest v8 Module function to getheapStatistics() for memory usage

--- Essential Notes/lesson two/core.js
+++ Essential Notes/lesson two/core.js
@@ -1,6 +1,7 @@
 // To Load Core or npm Modules(Custom JS files) to Our Program we Use the Require() function
 var path = require('path');         // getting the path module
 var util=require('util');           // getting the util module
+var v8= require('v8');              // magical v8 module 
 
 console.log(path.basename(__filename));     // this will return the base module name
 
@@ -8,4 +9,5 @@ var uploadsDir= path.join(__dirname, 'www','files', 'uploads');                /
 
 console.log(uploadsDir);                                                       // this prints out the uploadsDir with custom added directories
 //console.log(util);                                                              
-util.log(uploadsDir);                                                                 // there is a util. log function which gives us the 
+util.log(uploadsDir);                                                          // there is a util. log function which gives us the with date and time for the log
+console.log(v8.getHeapSpaceStatistics());                                      // these are some cool stuffs we can do with the v8 module  this JS memory Usage Stats
