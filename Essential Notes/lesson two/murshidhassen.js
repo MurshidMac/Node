e2620b0ad168f248434bfe3dfa587692ec24e1c4
MurshidMac <mh.murshidco.uk@outlook.com>
2017-05-12 23:24:09 +0530

    Event Emitter Object is added to it uses the PUbSUb Design pattern

--- Essential Notes/lesson two/murshidhassen.js
+++ Essential Notes/lesson two/murshidhassen.js
@@ -4,12 +4,15 @@
 // Pub Sub Means Publish and Subscribe Design Pattern Which used In Message Oriented Middlewares
 
 var event = require('events');              // event Module being called as an object
-var emiiter = new event.EventEmitter();     // Event Emitter is a Constructor and it needs to be called as an object
+var emitter = new event.EventEmitter();     // Event Emitter is a Constructor and it needs to be called as an object
+                                            // Instance of the event Emitter is created above
 
 // this is an emitter event     
 // this is an asynchronous callback function
 emitter.on('customEvent', function (message, status){
+    console.log(`${message} and ${status}`);    //  this gives us the message and the status code for the function
+});                                 // this is listner event where we can wire up the event in here
 
-});    // this is listner event where we can wire up the event in here
-
+// here I am calling back the emitter event in here
+emitter.emit('customEvent', "Murshid Hassen", 200);                     // this will fire up our custom event 
 
