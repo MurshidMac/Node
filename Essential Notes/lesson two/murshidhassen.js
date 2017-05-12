d76b613dea55a8b170d10af49ce3ebaa35e8e013
MurshidMac <mh.murshidco.uk@outlook.com>
2017-05-12 22:44:13 +0530

    added the asynchronous customEvent Through the eventClass

--- Essential Notes/lesson two/murshidhassen.js
+++ Essential Notes/lesson two/murshidhassen.js
@@ -3,6 +3,13 @@
 // One More Important this is that EventEmitter Uses a the Design Pattern Pub/Sub 
 // Pub Sub Means Publish and Subscribe Design Pattern Which used In Message Oriented Middlewares
 
-var event = require('events');  // event Module being called as an object
+var event = require('events');              // event Module being called as an object
 var emiiter = new event.EventEmitter();     // Event Emitter is a Constructor and it needs to be called as an object
+
+// this is an emitter event     
+// this is an asynchronous callback function
+emitter.on('customEvent', function (message, status){
+
+});    // this is listner event where we can wire up the event in here
+
 
