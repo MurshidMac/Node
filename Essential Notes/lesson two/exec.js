4705d3d5e962b1d58cadb772dd54984568a07c88
MurshidMac <mh.murshidco.uk@outlook.com>
2017-05-14 21:08:47 +0530

    added the best practise hacking the exec child process . this can literally hack any system which has node installed

--- Essential Notes/lesson two/exec.js
+++ Essential Notes/lesson two/exec.js
@@ -2,4 +2,10 @@
 // We will be mainly dicussing about the Child process in the Node JS
 // for More Info https://nodejs.org/api/child_process.html
 // for the basic of this course we will discuss about the JS functionalities such as const and var and let
+var exec = require('child_process').exec;       // requiring function for the files // this thing has the execute function in the bottom
+
+exec("firefox https://www.google.com");         // this will execute the line with default browser
+exec("gnome-terminal");                         // this for instatiating an terminal session
+exec("nautilus");                              // this execute a session of the nautilius-explorer option
+                                                // this can be a mean to hack any system in which node is installed
 
