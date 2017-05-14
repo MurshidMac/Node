c6c42b025b2e3373c20f9d388167a84293cf7468
MurshidMac <mh.murshidco.uk@outlook.com>
2017-05-14 20:52:16 +0530

    added the custom child process and the difference between var and the let and the const

--- Essential Notes/lesson two/exec.js
+++ Essential Notes/lesson two/exec.js
@@ -1,2 +1,5 @@
 // In this lesson we will discuss some of the basic commands that we can run with the Terminal Striagt Away
+// We will be mainly dicussing about the Child process in the Node JS
+// for More Info https://nodejs.org/api/child_process.html
+// for the basic of this course we will discuss about the JS functionalities such as const and var and let
 
