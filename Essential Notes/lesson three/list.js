43653e08a1bd7b7926cee5cd3d4513f9db6a43b4
MurshidMac <mh.murshidco.uk@outlook.com>
2017-05-19 14:43:51 +0530

    added the reading files with single thread

--- Essential Notes/lesson three/list.js
+++ Essential Notes/lesson three/list.js
@@ -3,7 +3,16 @@ var fs =require("fs");  // fs module for need works
 // we are creating an asynchronous read function 
 var files = fs.readdirSync('./lib');
 
+fs.readdirSync('./lib', function (err, vale){
+    if (err){
+        throw err
+    }
+    console.log(vale);
+})
+
+
 console.log(files);
+console.log("reading files finished");
 
 
 
