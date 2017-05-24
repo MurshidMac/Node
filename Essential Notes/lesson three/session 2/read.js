09f07a18431a13107192a19a2e60c198a479202a
MurshidMac <mh.murshidco.uk@outlook.com>
2017-05-25 00:00:59 +0530

    added readfileSync

--- Essential Notes/lesson three/session 2/read.js
+++ Essential Notes/lesson three/session 2/read.js
@@ -1,5 +1,5 @@
 var fs = require("fs"); // require the fs module
 
-var read = fs.readFileSyncSync("./lib/sayings.md", "UTF-8");  // this will be text
+var read = fs.readFileSync("./lib/sayings.md", "UTF-8");  // this will be text
 
 console.log(read);
