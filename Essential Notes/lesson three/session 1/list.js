1aa1fe02298ec4959e1a2363c64ddcf9b22e5daf
MurshidMac <mh.murshidco.uk@outlook.com>
2017-05-24 23:52:46 +0530

    added the second session as well

--- Essential Notes/lesson three/session 1/list.js
+++ Essential Notes/lesson three/session 1/list.js
@@ -1,7 +1,7 @@
 // we need the file system module to interact with whatever we have in the process function
 var fs =require("fs");  // fs module for need works
 // we are creating an asynchronous read function 
-var files = fs.readdirSync('./lib');
+//var files = fs.readdirSync('./lib');
 
 fs.readdirSync('./lib', function (err, vale){
     if (err){
