4624201a8d84033e1cfebeeffcc30019a9869b76
MurshidMac <mh.murshidco.uk@outlook.com>
2017-05-24 23:59:04 +0530

    added the read file function

--- Essential Notes/lesson three/session 2/read.js
+++ Essential Notes/lesson three/session 2/read.js
@@ -1,4 +1,5 @@
-var fs = require('fs'); // require the fs module
+var fs = require("fs"); // require the fs module
 
 var read = fs.readFileSyncSync("./lib/sayings.md", "UTF-8");  // this will be text
 
+console.log(read);
