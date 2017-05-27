b8d539c54b4970ad446e3560748b94f6e1a9b71c
MurshidMac <mh.murshidco.uk@outlook.com>
2017-05-27 23:19:46 +0530

    added the modified link for FS module

--- Essential Notes/lesson three/session 2/read2.js
+++ Essential Notes/lesson three/session 2/read2.js
@@ -1,6 +1,6 @@
 var read= require("fs");    // file system module
 var contents= read.readFileSync('./lib/sayings.md', "UTF-8");
-var contenstWithoutTextEncoding = read.readdirSync('./lib/sayings/md');
+var contenstWithoutTextEncoding = read.readFileSync('./lib/sayings/md');
 
 console.log(contents);
 console.log(contenstWithoutTextEncoding);
