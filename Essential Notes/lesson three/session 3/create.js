62eb5e40b213ea79e5d2e1feb94abf48e5280c75
MurshidMac <mh.murshidco.uk@outlook.com>
2017-05-30 00:08:23 +0530

    added the markdown function

--- Essential Notes/lesson three/session 3/create.js
+++ Essential Notes/lesson three/session 3/create.js
@@ -1,6 +1,19 @@
 var fs = require("fs");
 
+var md =`
+Sample data written to the file
+===============================
 
+*Murshid
+*hassen
+
+Lets use JS for better performance
+==================================
+
+hi
+
+
+`;
 
 
 
