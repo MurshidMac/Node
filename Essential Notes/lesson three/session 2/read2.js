0e275815b34ad3eaa7e95a9177c37a35a09ed69a
MurshidMac <mh.murshidco.uk@outlook.com>
2017-05-29 23:55:51 +0530

    added some description

--- Essential Notes/lesson three/session 2/read2.js
+++ Essential Notes/lesson three/session 2/read2.js
@@ -5,6 +5,8 @@ var path= require("path");
 
 //console.log(contents);
 //console.log(contenstWithoutTextEncoding);
+// here what we do is we use several file system to combine many file system to read all from the directory
+// function readdir is to read the directory and the currently to load the path
 fs.readdir("./lib", function (err, files){
     files.forEach(function (fileName){
         var file= path.join(__dirname, "lib", fileName);    // this creates a fullpath to the current directory
