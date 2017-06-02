81ef98afb91efddcf62c220a8c6315d0b49779e6
MurshidMac <mh.murshidco.uk@outlook.com>
2017-06-02 23:49:15 +0530

    added the directory for the link

--- Essential Notes/lesson three/session 4/directory.js
+++ Essential Notes/lesson three/session 4/directory.js
@@ -1,15 +1,16 @@
-var fs= require('fs');
+var fs = require('fs');
 
-if(fs.exists("lib")){
+if (fs.exists("lib")) {
     console.log("Directory Already there");
+} else {
+
+    fs.mkdir("lib", function (err) {
+        if (err) {
+            console.log("throwing an error" + err);
+        }
+        else {
+            console.log("Directory Created");
+        }
+    });
+
 }
-
-fs.mkdir("lib", function (err){
-    if(err){
-        console.log("throwing an error"+ err);
-    }
-    else{
-        console.log("Directory Created");
-    }
-});
-
