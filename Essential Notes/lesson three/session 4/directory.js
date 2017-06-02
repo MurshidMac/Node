e99a3fd673985a0847b94612002dff77f883bb4b
MurshidMac <mh.murshidco.uk@outlook.com>
2017-06-02 23:47:29 +0530

    added the directory existing function

--- Essential Notes/lesson three/session 4/directory.js
+++ Essential Notes/lesson three/session 4/directory.js
@@ -1,5 +1,9 @@
 var fs= require('fs');
 
+if(fs.exists("lib")){
+    console.log("Directory Already there");
+}
+
 fs.mkdir("lib", function (err){
     if(err){
         console.log("throwing an error"+ err);
