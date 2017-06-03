64ae46bbf67ca603ecfa9391caa75b849e2c484b
MurshidMac <mh.murshidco.uk@outlook.com>
2017-06-03 21:58:46 +0530

    added the rename file

--- Essential Notes/lesson three/session 5/rename.js
+++ Essential Notes/lesson three/session 5/rename.js
@@ -3,9 +3,9 @@ var fs= require("fs");
 fs.renameSync("./lib/project-config.js","./lib/project-config.json");
 // great to use the same rename file function to replace a folder from one path to another
 
-console.log("Configure .json file is renamed");
+console.log("Configure.json file is renamed");
 
-fs.rename('./lib/notes.md', './notes.md', function (err) {
+fs.rename("./lib/notes.md","./notes.md", function (err) {
     if(err){
         console.log(err);
     }else{
