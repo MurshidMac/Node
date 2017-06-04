e22e88922e3b41feb4b159035b70661f5e350089
MurshidMac <mh.murshidco.uk@outlook.com>
2017-06-04 23:28:22 +0530

    commented the remove function

--- Essential Notes/lesson three/session 5/remove.js
+++ Essential Notes/lesson three/session 5/remove.js
@@ -3,11 +3,13 @@
 var fs= require("fs");
 // we need to remove the files from the project 
 // Since its sync it has to be try catched
+/*
 try{
     fs.unlinkSync("./remove.json");
 }catch(err){
     console.log(err);
 }
+*/
 // this is the async version
 fs.unlink("./unlinkAsync.json", function (err){
     if(err){
