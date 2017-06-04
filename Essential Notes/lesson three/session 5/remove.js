a296ba2f8249c05a193da9da93cc52892d4dba7d
MurshidMac <mh.murshidco.uk@outlook.com>
2017-06-04 23:21:17 +0530

    added the unlink option

--- Essential Notes/lesson three/session 5/remove.js
+++ Essential Notes/lesson three/session 5/remove.js
@@ -1,6 +1,14 @@
 // we will be covering the remove function from the config.json file
 // we are gonna remove two unwanted links from the file system
 var fs= require("fs");
+// we need to remove the files from the project 
+// Since its sync it has to be try catched
+try{
+    fs.unlinkSync("./remove.json");
+}catch(err){
+    console.log(err);
+}
+
 
 
 
